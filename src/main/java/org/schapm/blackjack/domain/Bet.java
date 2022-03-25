b5bc0508bb11044a6280eebfddaf83789a0a584b
schapm <ozuhouz@pm.me>
2022-03-25 18:10:03 +0000

    ADD: Bet class

--- src/main/java/org/schapm/blackjack/domain/Bet.java
+++ src/main/java/org/schapm/blackjack/domain/Bet.java
@@ -1,10 +1,61 @@
 package org.schapm.blackjack.domain;
 
+import org.schapm.blackjack.game.Player;
+
 /**
  *
  * @author schapm
  */
 
 public class Bet {
+
+    private final Player player;
+    private final Player dealer;
+    private final int betSize;
+
+    public Bet(Player player, Player dealer, int betSize) {
+        this.player = player;
+        this.dealer = dealer;
+        this.betSize = betSize;
+    }
+
+    public boolean placeBet() {
+        // First check both player & dealer have enough balance
+        if (player.getBankroll().getBalance() - betSize >= 0
+                && dealer.getBankroll().getBalance() - betSize >= 0) {
+
+            player.getBankroll().remove(betSize);
+            dealer.getBankroll().add(betSize);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public void win() {
+        // Return stake and winnings
+        player.getBankroll().add(betSize * 2);
+        dealer.getBankroll().remove(betSize * 2);
+    }
+
+    public void draw() {
+        // Return stake
+        player.getBankroll().add(betSize);
+        dealer.getBankroll().remove(betSize);
+    }
+
+    public int getBet() {
+        return this.betSize;
+    }
+
+    public Player getBalanceNotEnough() {
+        // Who doesn't have enough balance to settle the bet
+        if (player.getBankroll().getBalance() - betSize < 0) {
+            return player;
+        }
+
+        return dealer;
+    }
 
 }
