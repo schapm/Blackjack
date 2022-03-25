eb0dfd18b8d70a10694e61131ba91740fe3bbad5
schapm <ozuhouz@pm.me>
2022-03-25 10:55:33 +0000

    ADD: Player class

--- src/main/java/org/schapm/blackjack/game/Player.java
+++ src/main/java/org/schapm/blackjack/game/Player.java
@@ -1,10 +1,28 @@
 package org.schapm.blackjack.game;
 
+import org.schapm.blackjack.domain.Bankroll;
+
 /**
  *
  * @author schapm
  */
 
 public class Player {
+
+    private final String name;
+    private final Bankroll bankroll;
+
+    public Player(String name, Bankroll bankroll) {
+        this.name = name;
+        this.bankroll = bankroll;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Bankroll getBankroll() {
+        return bankroll.getBankroll();
+    }
 
 }
