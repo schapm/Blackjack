5ef8a1834fe8ead3d4a10472099412798a4828eb
schapm <ozuhouz@pm.me>
2022-03-28 21:34:57 +0100

    ADD: Text class

--- src/main/java/org/schapm/blackjack/ui/Text.java
+++ src/main/java/org/schapm/blackjack/ui/Text.java
@@ -7,4 +7,16 @@ package org.schapm.blackjack.ui;
 
 public class Text {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+
+    public static final String UNICODE_BLACK_SPADE = "♠";
+    public static final String UNICODE_BLACK_HEART = "♥";
+    public static final String UNICODE_BLACK_CLUB = "♣";
+    public static final String UNICODE_BLACK_DIAMOND = "♦";
+
+    public static String colorText(String text, String color) {
+        return color + text + ANSI_RESET;
+    }
 }
