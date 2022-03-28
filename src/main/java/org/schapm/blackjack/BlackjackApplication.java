0d62e9115a2434a5a9fc633b7350245fef61ba45
schapm <ozuhouz@pm.me>
2022-03-28 13:51:17 +0100

    ADD: Main method

--- src/main/java/org/schapm/blackjack/BlackjackApplication.java
+++ src/main/java/org/schapm/blackjack/BlackjackApplication.java
@@ -1,5 +1,9 @@
 package org.schapm.blackjack;
 
+import org.schapm.blackjack.domain.Bankroll;
+import org.schapm.blackjack.game.Game;
+import org.schapm.blackjack.game.Player;
+
 /**
  *
  * @author schapm
@@ -8,6 +12,14 @@ package org.schapm.blackjack;
 public class BlackjackApplication {
 
     public static void main(String[] args) {
+        // New player with default bankroll
+        Player player = new Player("John", new Bankroll());
+        // New dealer with £1,000 bankroll
+        Player dealer = new Player("The House", new Bankroll(1_000));
 
+        // Start new game with player and dealer
+        Game game = new Game(player, dealer);
+        // Start game
+        game.start();
     }
 }
